88806a0f6bac841498be8b9ca79b5bc969bfdea6
Devansh-Maheshwari <caredevansh@gmail.com>
2024-10-10 15:21:20 +0530

    feat:added task 1 and routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const mongoose=require('mongoose');
-
+const fetchCryptoData=require('./controllers/fetchCryptoData')
+const cryptoRoutes = require('./routes/cryptoRoutes');
+const schedule = require('node-schedule');
 require('dotenv').config();
 
 const app=express();
@@ -8,6 +10,20 @@ const app=express();
 const PORT=process.env.PORT || 3000;
 
 require("./config/database").connect();
+
+app.use('/',cryptoRoutes)
+
+//Task 1 :Implement a background job that will fetch the current price in USD,That will run every 2hrs
+
+const job = schedule.scheduleJob('0 */2 * * *', async () => {
+    try {
+        await fetchCryptoData();
+        console.log('Crypto data fetched successfully!');
+    } catch (error) {
+        console.error("Error fetching crypto data: ", error);
+    }
+});
+
 app.listen(PORT ,()=>{
     console.log(`app running at port ${PORT}`);
 })
